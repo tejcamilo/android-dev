b5bd9ca3cfc4f090cfb1fbf8237aba754b7362e3
tejcamilo <ctejada@twilio.com>
2025-04-16 00:08:29 -0500

    Implementing activity state

--- Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,18 +13,32 @@ class MainActivity : AppCompatActivity() {
     var running = false
     var offset: Long = 0
 
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         stopwatch = findViewById(R.id.stopwatch)
 
+        // Restore previous state
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+        }
+
         val startButton = findViewById<Button>(R.id.start_button)
         startButton.setOnClickListener {
             if (!running) {
                 setBaseTime() // start stopwatch from the correct time
                 stopwatch.start()
-                running = false
+                running = true
             }
         }
 
@@ -42,6 +54,13 @@ class MainActivity : AppCompatActivity() {
             offset = 0
             setBaseTime() // set stopwatch time back to 0
         }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
     }
 
     fun setBaseTime() {
