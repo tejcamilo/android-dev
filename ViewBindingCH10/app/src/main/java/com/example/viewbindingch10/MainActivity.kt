106e3def06f49ed7d08d7fb9681ac5101b8b0a78
tejcamilo <ctejada@twilio.com>
2025-04-26 21:01:40 -0500

    Implement view binding instead of findViewById

--- ViewBindingCH10/app/src/main/java/com/example/viewbindingch10/MainActivity.kt
+++ ViewBindingCH10/app/src/main/java/com/example/viewbindingch10/MainActivity.kt
@@ -1,20 +1,86 @@
 package com.example.viewbindingch10
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.viewbindingch10.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    var running = false
+    var offset: Long = 0
+
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Restore previous state
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running) {
+                binding.stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                binding.stopwatch.start()
+            } else setBaseTime()
         }
+
+        binding.startButton.setOnClickListener {
+            if (!running) {
+                setBaseTime() // start stopwatch from the correct time
+                binding.stopwatch.start()
+                running = true
+            }
+        }
+
+        binding.pauseButton.setOnClickListener {
+            saveOffset() // save the time on the stopwatch
+            binding.stopwatch.stop()
+            running = false
+        }
+
+        binding.resetButton.setOnClickListener {
+            offset = 0
+            setBaseTime() // set stopwatch time back to 0
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (running) {
+            saveOffset()
+            binding.stopwatch.stop()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (running) {
+            setBaseTime()
+            binding.stopwatch.start()
+            offset = 0
+        }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putLong(OFFSET_KEY, offset)
+        savedInstanceState.putBoolean(RUNNING_KEY, running)
+        savedInstanceState.putLong(BASE_KEY, binding.stopwatch.base)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    fun setBaseTime() {
+        binding.stopwatch.base = SystemClock.elapsedRealtime() - offset
+    }
+
+    fun saveOffset() {
+        offset = SystemClock.elapsedRealtime() - binding.stopwatch.base
     }
 }
